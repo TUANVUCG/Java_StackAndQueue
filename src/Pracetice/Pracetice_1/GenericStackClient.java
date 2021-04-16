0b7c07dbe0d1f29c81a1c905aaeda824c6925acb
TUAN VU <tuanvuambition@gmail.com>
2021-04-16 11:55:44 +0700

    practice 1

---
+++ src/Pracetice/Pracetice_1/GenericStackClient.java
@@ -0,0 +1,40 @@
+package Pracetice.Pracetice_1;
+
+public class GenericStackClient {
+
+    private static void stackOfIString() {
+        MyGenericStack<String> stack = new MyGenericStack<>();
+        stack.push("one");
+        stack.push("two");
+        stack.push("three");
+        stack.push("four");
+        stack.push("five");
+
+        System.out.println("Size is : " + stack.size());
+        while (!stack.isEmpty()) {
+            System.out.printf("%s", stack.pop());
+        }
+    }
+
+
+    private static void stackOfIntegers() {
+        MyGenericStack<Integer> stack = new MyGenericStack<>();
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        System.out.println("Size of Integer Stack : " + stack.size());
+        while (!stack.isEmpty()) {
+            System.out.printf("%d", stack.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        stackOfIntegers();
+        System.out.println("\n2.Stack of String ");
+        stackOfIString();
+    }
+}
+
---
+++ src/Pracetice/Pracetice_1/MyGenericStack.java
@@ -0,0 +1,40 @@
+package Pracetice.Pracetice_1;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList<>();
+    }
+    // phuong thuc push
+    public void push(T element){
+        stack.addFirst(element);
+    }
+
+
+    // phuong thuc pop
+    public T pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+    // phuong thuc size
+    public int size(){
+        return stack.size();
+    }
+
+    // phuong thuc isEmpty
+    public boolean isEmpty(){
+        if(stack.size()==0){
+            return true;
+        }
+        return false;
+    }
+
+
+}
